a0685affe321287c13d7a6d27f6fd8823b7bed9d
Drake Fish <drakefish@Drakes-MacBook-Pro.local>
2018-12-10 13:27:38 -0600

    added editing, and deleting functionality. extracted and refactored code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:edit, :update, :show, :destory]
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def index
     @posts = Post.all
   end
@@ -20,21 +26,24 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:notice] = "Post was successfully updated!"
       redirect_to post_path(@post)
     else
       render :edit
     end
+   end
+
+   def destroy
+     @post.destroy
+     flash[:notice] = "Post was successfully deleted!"
+     redirect_to posts_path
    end
 
 private
