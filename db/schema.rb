bf132f8beee67deda7df636e2f3df9864096fe66
Drake Fish <drakefish@Drakes-MacBook-Pro.local>
2018-12-10 14:06:57 -0600

    added users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_173729) do
+ActiveRecord::Schema.define(version: 2018_12_10_200228) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
