bd8ce09b91db90039f030d35db5e6a6a4dfd02bb
Drake Fish <drakefish@Drakes-MacBook-Pro.local>
2018-12-11 10:30:18 -0600

    add edit update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :users, except: [:new]
   root 'pages#home'
   # get 'about', to: 'welcome#about'
   get 'signup', to: 'users#new'
 
-  post 'users', to: 'users#create'
 end
